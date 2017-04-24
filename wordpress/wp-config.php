dfb4d533fc7e0aa7f9c7008ff8a2ba2b7200a519
Joni Weiss <me@joniweiss.com>
2017-04-24 12:47:53 -0600

    Imported live site

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -63,7 +63,7 @@ define('NONCE_SALT',       '<83<|_z(S]pL[.1|;FtFdZed963DuPii._po%7ei~q;uYLsGvvb7
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_';
+$table_prefix  = 'wx_';
 
 /**
  * For developers: WordPress debugging mode.
---
+++ wordpress/wp-content/themes/wp-forge/js/wpforge-functions.js
@@ -0,0 +1,46 @@
+/*
+ * @since WP-Forge 5.5.1.7
+ * @version 6.2.1
+ */
+
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+jQuery(document).ready(function($) {
+
+	// Add button class to certain buttons in the theme
+	jQuery('input[type="submit"]').addClass('button');
+	
+	// Adds flex video to embeded video: http://foundation.zurb.com/docs/components/flex-video.html
+	jQuery('iframe[src*="vimeo.com"]').wrap('<div class="flex-video widescreen vimeo" />');
+	jQuery('iframe[src*="youtube.com"],iframe[src*="dailymotion.com"]').wrap('<div class="flex-video widescreen" />');
+	
+	// BackToTop Button: Controls the fade in of the BacktoTop Button
+	jQuery(window).load(function() {
+		jQuery("#topofpage").hide().removeAttr("href");
+		if (jQuery(window).scrollTop() != "0")
+			jQuery("#backtotop").fadeIn("slow")
+		var scrollDiv = jQuery("#backtotop");
+		jQuery(window).scroll(function(){
+			if (jQuery(window).scrollTop() == "0")
+				jQuery(scrollDiv).fadeOut("slow")
+			else
+				jQuery(scrollDiv).fadeIn("slow")
+		});	
+	});	
+	// BacktoTop
+	jQuery('#backtotop').click(function(){
+		jQuery('html, body').animate({
+		scrollTop: jQuery('body').offset().top
+		}, 1000); // Change this value to control the speed of the scroll back to the top of the page.		   
+	});
+
+	// Remove empty P tags created by WP inside of Accordion and Orbit - Thanks to JointsWP - added 6.1.1
+    jQuery('.accordion p:empty, .orbit p:empty, .accordion br, .entry-content p:empty, .entry-content br').remove();
+
+    // Add clearfix class to gallery
+	jQuery('.gallery').addClass('clearfix');
+
+// end loading all functions  
+   
+});
